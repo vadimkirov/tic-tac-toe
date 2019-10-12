659a81329cbfa7ce83577352896603db9a93a9c4
Vadim Kirov <vadimkirov@ya.ru>
2019-10-12 20:02:35 +0300

    feat: add task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -18,76 +18,106 @@ class TicTacToe {
         this.turn = this.huPlayer;
         this.canMoves = N_size * N_size;
         this.stopPlay = this.canMoves <= 0 ;
+        this.winner = null;
     }
+
+
 
     getCurrentPlayerSymbol() {
         return this.turn;
     }
+
+
 
     nextTurn(rowIndex, columnIndex) {
 
         let currentSymbol = this.getFieldValue(rowIndex, columnIndex);
         if(currentSymbol === null){
             this.matrix[rowIndex][columnIndex] = this.turn;
-            let yyyyyyyy = this.isFinished();
             this.stopPlay = (--this.canMoves === 0);
+
+            if(this.getWinner() !== null) this.winner = this.turn;
+
             if(this.turn === this.huPlayer) {
-                        this.turn = this.aiPlayer;
-                    } else {
-                        this.turn = this.huPlayer;
-                    }
+                this.turn = this.aiPlayer;
+            } else {
+                this.turn = this.huPlayer;
+            }
         }
     }
+
+
 
     isFinished() {
         return (this.isDraw() || (this.getWinner() !== null));
     }
 
+
+
     getWinner() {
+
+        if(this.winner !== null) return this.winner;
         let matrixSize = this.matrix.length;
 
+        let columnWin = 0;
         let rowWin = 0;
-
         let diagWinLeftToRight = 0;
         let diagWinRightToLeft = 0;
-        for (let i = 0; i < matrixSize; i++) {
 
-            let columnWin = 0;
+        for (let i = 0; i < matrixSize; i++){
             for(let j = 0; j < matrixSize; j++){
                 if(this.matrix[i][j] === this.turn){
-                    columnWin++;
+                    rowWin++;
                 }
                 if (i === j   &&  this.matrix[i][j] === this.turn ) {
                     diagWinLeftToRight++;
-                    diagWinRightToLeft++;
                 }
             }
-            if (columnWin === matrixSize) return this.turn;
-            if(this.matrix[i][0] === this.turn){
-                rowWin++;
+            if(rowWin === matrixSize) {
+                return this.turn;
+            }else {
+                rowWin = 0;
             }
         }
-        if (rowWin === matrixSize ) return this.turn;
 
-        if (this.matrix[0][0] === this.turn) diagWinLeftToRight++;
-        if (this.matrix[matrixSize-1][matrixSize-1] === this.turn) diagWinLeftToRight++;
+        for (let j = 0; j < matrixSize; j++){
+            for(let i = 0; i < matrixSize; i++){
+                if(this.matrix[i][j] === this.turn){
+                    columnWin++;
+                }
+            }
+
+            if(columnWin === matrixSize) {
+                return this.turn;
+            }else {
+                columnWin = 0;
+            }
+        }
+
         if (diagWinLeftToRight === matrixSize) return this.turn;
 
         if (this.matrix[0][matrixSize-1] === this.turn) diagWinRightToLeft++;
+        if (this.matrix[1][1] === this.turn) diagWinRightToLeft++;
         if (this.matrix[matrixSize-1][0] === this.turn) diagWinRightToLeft++;
         if (diagWinRightToLeft === matrixSize) return this.turn;
 
-
         return null;
     }
+
+
+
 
     noMoreTurns() {
         return this.stopPlay;
     }
 
+
+
     isDraw() {
-        return (this.noMoreTurns() || (this.getWinner() !== null)) ;
+        return (this.noMoreTurns() && (this.getWinner() === null)) ;
     }
+
+
 
     getFieldValue(rowIndex, colIndex) {
         return this.matrix[rowIndex][colIndex];
@@ -95,108 +125,3 @@ class TicTacToe {
 }
 
 module.exports = TicTacToe;
-//
-// let game;
-// let rez;
-// game = new TicTacToe();
-//
-// game.nextTurn(1, 0);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(2, 2);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(0, 2);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(1, 0);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(2, 2);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(1, 1);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(0, 2);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(2, 2);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(0, 2);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(0, 2);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(1, 1);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(1, 2);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(1, 1);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(1, 2);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(2, 0);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(0, 0);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(2, 0);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(1, 1);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(0, 1);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(1, 2);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(1, 1);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(0, 1);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(2, 0);
-//
-// rez= game.isDraw();  //  ).to.equal(false);
-//
-// game.nextTurn(2, 1);
-//
-// rez= game.isDraw();  //   ).to.equal(true);
-//
-//
-//
-//
-// let ccccccccccc = rez;
